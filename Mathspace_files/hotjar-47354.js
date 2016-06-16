a82175550f46f34743af635b891d75aac73817fa
ShadyGuy56 <ShadyGuy56@gmail.com>
2016-06-16 16:05:51 +0800

    Added files
    test

---
+++ Mathspace_files/hotjar-47354.js
@@ -0,0 +1,10 @@
+window.hjSiteSettings = window.hjSiteSettings || {"testers_widgets":[],"surveys":[],"heatmaps":[],"recording_capture_keystrokes":true,"polls":[],"site_id":47354,"record_targeting_rules":[{"negate":false,"pattern":"signup\/teacher","match_operation":"contains","component":"url"},{"negate":false,"pattern":"signup\/personal","match_operation":"contains","component":"url"},{"negate":false,"pattern":"signup\/student","match_operation":"contains","component":"url"}],"forms":[],"record":false,"r":0.1547357497,"deferred_page_contents":[]};
+
+window.hjBootstrap = window.hjBootstrap || function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+    window.hjBootstrap = function() {};
+};
+
+hjBootstrap('https://script.hotjar.com/modules-e12a7ede294d22ffa3a8a711155156e3.js');
